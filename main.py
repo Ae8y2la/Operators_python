b3722f368a9453118793a63fd83ed4a7ba0816e1
Ae8y2la <aeylanaseer@gmail.com>
2025-03-04 18:31:07 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,108 @@
+# ---------------------------------------OPERATORS WITH THEIR EXAMPLES --------------------------------
+
+#(1) **Arithmetic Operators**
+
+x = 20
+y = 5
+
+# Printing arithmetic operators with its operators!
+
+print("Addition:", x + y)       
+print("Subtraction:", x -y)     
+print("Multiplication:", x * y)  
+print("Division:", x / y)        
+print("Modulus:", x % y)         
+print("Exponentiation:", x ** y) 
+print("Floor Division:", x // y) 
+
+# ******************************************************************
+
+# (2) **Assignment Operators**
+x = 20
+y = 3
+
+# Printing assignment operators with their results
+
+x += y  
+print("x += y:", x)  
+
+x -= y  
+print("x -= y:", x) 
+
+x *= y  
+print("x *= y:", x)  
+# ******************************************************************
+
+#(3) **Comparison Operators**
+
+x = 20
+y = 2
+
+# Printing comparison operators with its operators!
+
+print("Equal:", x == y)        
+print("Not Equal:", x!= y)     
+print("Greater Than:", x > y)       
+print("Less Than:", x < y)        
+print("Greater Than or Equal To:", x >= y)  
+print("Less Than or Equal To:", x <= y)  
+
+# ******************************************************************
+
+#(4) **Logical Operators**
+
+x = True
+y = False
+
+# Printing logical operators with its operators!
+
+print("AND:", x and y)       
+print("OR:", x or y)        
+print("NOT:", not y)      
+
+# ******************************************************************
+
+#(5) **Identity Operators**
+
+x = 10
+y = 10
+z = 4
+
+# Printing identity operators with its operators!
+
+print("Equal:", x is y)   
+print("Equal:", x is z)
+print("Not Equal:", x is not y)
+
+# ******************************************************************
+
+#(6) **Membership Operators**
+
+x = [1, 2, 3, 4, 5]
+
+# Printing membership operators with its operators!
+
+print("1 in x:", 1 in x)        
+print("6 not in x:", 6 not in x)
+
+# ******************************************************************
+
+
+#(7) **Bitwise Operators**
+
+x = 20
+y = 7
+
+# Printing bitwise operators with its operators!
+
+print("Binary AND:", bin(x & y))      
+print("Binary OR:", bin(x | y))       
+print("Binary XOR:", bin(x ^ y))      
+print("Binary NOT:", bin(~x))        
+print("Binary Left Shift:", bin(x << 2))
+print("Binary Right Shift:", bin(x >> 2))
+
+# ******************************************************************
+
+# --------------------------------------THE-END----------------------------------------------------------------
+
